6e375c9805e9217ec2520fdea8b74fc0610c9453
m0cchi <boom.boom.planet@gmail.com>
2018-09-16 21:14:51 +0900

    support ltsv

--- syslog/bin/run.sh
+++ syslog/bin/run.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-cat <<EOF > /etc/rsyslog.conf
-# rsyslog configuration file
-
-global(workDirectory="/var/lib/rsyslog")
-module(load="builtin:omfile" Template="RSYSLOG_TraditionalFileFormat")
-
-module(load="imudp")
-input(type="imudp" port="514")
-
-local5.*    /var/log/nginx_access_log
-EOF
+mkdir -p /var/lib/rsyslog
+cat /root/bin/rsyslog.conf > /etc/rsyslog.conf
 
 /usr/sbin/rsyslogd -n
